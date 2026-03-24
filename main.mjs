0f992246406d2da01825a75add98374a5f022a10
Oliver <Oliversto@uia.no>
2026-03-24 13:33:09 +0100

    Updated to you can work in different functions

--- main.mjs
+++ main.mjs
@@ -1,31 +1,65 @@
 const baseURL = "https://batlearena.onrender.com";
 const regTeamURL = `${baseURL}/teams`;
+const createMatchURL =`${baseURL}/matches`;
 
 
 //Post /teams
 
 const myTeam = {
-"name": "TrashPandas",
-"members":["Alexandra","Ronja","Kjetil","Oliver","Alena"],
-"password": "Pandas"
+name: "TrashPandas",
+members:["Alexandra","Ronja","Kjetil","Oliver","Alena"],
+password: "Pandas"
 }
 
 
 const config = {
     method: "POST",
     headers : {
+        Authentification: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVHJhc2hQYW5kYXMiLCJjcmVhdGVkIjoiMjAyNi0wMy0yNFQxMTo1NDoxNi43MDdaIn0.QgRWBIfIAIgWlGtJFdTCZ2Ehr0zvd6hgb7UlBT3qJ60',
         Accept: "application/json",
         "Content-Type": "application/json",
-        Authentification: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiVHJhc2hQYW5kYXMiLCJjcmVhdGVkIjoiMjAyNi0wMy0yNFQxMTo1NDoxNi43MDdaIn0.QgRWBIfIAIgWlGtJFdTCZ2Ehr0zvd6hgb7UlBT3qJ60',
-        
     },
 body: JSON.stringify(myTeam),
 }
 
-let response = await fetch (regTeamURL, config);
+function register(){
+    let conf = {...config};
+    conf.method = "POST"
+    conf.body = JSON.stringify(myTeam),
+
+
+let response = await fetch (regTeamURL, conf);
 if (response.status == 200){
-    let token = await response.json();
-    console.log(token);
+    let matchInfo = await response.json();
+    console.log(matchInfo);
+}
+else{
+    console.error("Could not create match");
+    console.error(response);
+}
 
 }
+
+function makeMatch(){
+    await makeMove(matchID,MOVE){
+        let conf = ( ...config )};
+        conf.method = "POST"
+(response.status == 200){
+    
+}
+    }
+ 
+}
+
+function joinMatch(matchID){
+
+}
+
+function getMatches(){
+    let conf = {...config};
+    conf.method = "POST"
+    conf.body = "GET"
+}
+
+
 
